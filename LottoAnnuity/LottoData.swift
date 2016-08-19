73e4845eec11b9530e0bb3af0caabf151a0fb5b5
Lamar Greene <github@lamargreene.com>
2016-08-19 10:18:43 -0500

    Init Commit
    This is the third initial commit. I was previously NOT properly
    utilizing Github... I went down some paths that were not easily
    recoverable. Adding third party libraries for bad reasons, etc...

---
+++ LottoAnnuity/LottoData.swift
@@ -0,0 +1,122 @@
+//
+//  LottoData.swift
+//  lotto value
+//
+//  Created by Lamar Greene on 8/13/16.
+//  Copyright © 2016 Lamar Greene. All rights reserved.
+//
+
+import Foundation
+
+var installmentPayments = [String]()
+
+struct LottoChoice {
+    let name: String
+    let initialPercent: Double
+    let percentIncrease: Double
+    var jackpot: Double
+    let jackpotURL: String
+    let stringSeparator1: String
+    let stringSeparator2: String
+}
+
+var MEGA = LottoChoice(name: "mega", initialPercent: 0.0150514350803, percentIncrease: 1.05, jackpot: 15000000.0, jackpotURL: "http://www.megamillions.com", stringSeparator1: "<div class=\"home-next-drawing-estimated-jackpot-dollar-amount\">", stringSeparator2: "</div>")
+var POWER = LottoChoice(name: "power", initialPercent: 0.017830099134, percentIncrease: 1.04, jackpot: 40000000.0, jackpotURL: "http://www.powerball.com", stringSeparator1: "<h1>$", stringSeparator2: "&nbsp;")
+
+var megaComplete = false
+var powerComplete = false
+
+// should probably put a completion handler on here so updateDate() does not fire until this is complete
+func formatLotterJackpotText(forLottery: LottoChoice) -> String {
+    let kerry = NSString(string: "\(forLottery.jackpot)" as String).components(separatedBy: ".")
+  
+        return "\(kerry[0])"
+
+}
+
+
+func getLotteryJackpotValue(lottery: LottoChoice, completion: (result: Bool) -> Void) {
+    
+    if let url = URL(string: lottery.jackpotURL) {
+        
+        let request = NSMutableURLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            var message = ""
+            
+            if error != nil {
+                print(error)
+            } else {
+                
+                if let unwrappedData = data {
+                    
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    print(dataString)
+                    var stringSeparator = lottery.stringSeparator1
+                    
+                    if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                        
+                        if contentArray.count > 1 {
+                            
+                            stringSeparator = lottery.stringSeparator2
+                            
+                            print(contentArray[0])
+                            
+                            let newContentArray = contentArray[1].components(separatedBy: stringSeparator)
+                            
+                            if newContentArray.count > 1 {
+                                
+                                message = String(newContentArray[0]+"000000")
+                                
+                                if lottery.name == "mega" {
+                                    
+                                    MEGA.jackpot = Double(message)!
+                                    
+                                }else if lottery.name == "power"{
+                                
+                                    POWER.jackpot = Double(message)!
+                                    
+                                }
+                                
+                                completion(result: true)
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+            if message == "" {
+                
+                message = "The lotto data could not be found. Please try again."
+                
+            }
+            
+            DispatchQueue.main.sync(execute: {
+                
+                //self.resultLabel.text = message
+                
+            })
+            
+        }
+        
+        task.resume()
+        
+    } else {
+        
+        //resultLabel.text = "The weather there couldn't be found. Please try again."
+        
+    }
+    
+}
+
+
+
+
+
---
+++ LottoAnnuity/ViewController.swift
@@ -0,0 +1,183 @@
+//
+//  ViewController.swift
+//  LottoAnnuity
+//
+//  Created by Lamar Greene on 12/17/15.
+//  Copyright © 2015 Lamar Greene. All rights reserved.
+//
+
+import UIKit
+
+extension Float {
+    var asLocalCurrency: String{
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = NSLocale.current
+        formatter.maximumFractionDigits = 0
+        return formatter.string(from: self)!
+    }
+}
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var allPayments = ""
+    var currentPayment = ""
+    var selectedLottery = MEGA
+    var printInstallment: Float = 0.0
+    var firstPayment: Float = 0.0
+    
+    
+    @IBOutlet weak var lotteryLabel: UILabel!
+    
+    @IBOutlet weak var lotteryJackpot: UITextField!
+    
+    @IBOutlet weak var annuityText: UILabel!
+    
+    @IBOutlet weak var lotterySelection: UISegmentedControl!
+    
+    @IBAction func setLottery(sender: AnyObject) {
+        if lotterySelection.selectedSegmentIndex == 0 {
+            selectedLottery = MEGA
+            lotteryJackpot.text = formatLotterJackpotText(forLottery: selectedLottery)
+            setPaymentText()
+        } else {
+            selectedLottery = POWER
+            lotteryJackpot.text = formatLotterJackpotText(forLottery: selectedLottery)
+            setPaymentText()
+        }
+        updateData()
+    }
+    
+    @IBOutlet weak var stepperLabel: UILabel!
+    
+    @IBOutlet weak var stepperControl: UIStepper!
+    
+    @IBAction func stepperCount(sender: AnyObject) {
+        stepperLabel.text = String(Int(stepperControl.value))
+        
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        getLotteryJackpotValue(lottery: MEGA) { (result) in
+            self.updateData()
+        }
+        
+        getLotteryJackpotValue(lottery: POWER) { (result) in
+            self.updateData()
+        }
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //let kerry = NSString(string: "\(MEGA.jackpot)" as String).components(separatedBy: ".")
+        lotteryJackpot.text = "0"
+        
+        self.stepperLabel.text = String(Int(self.stepperControl.value))
+        self.lotteryLabel.text = "$0"
+        self.lotteryJackpot.delegate = self
+        self.lotteryJackpot.addTarget(self, action: #selector(ViewController.setPaymentText), for: UIControlEvents.editingChanged)
+        self.lotteryJackpot.textColor = UIColor.red  //(hue: 0, saturation: 0, brightness: 0, alpha: 0)
+        
+    }
+
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+       // print(lotteryJackpot.text)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func updateData(){
+        allPayments = ""
+        calcs()
+    }
+
+    
+    func setPaymentText(){
+        if lotteryJackpot.text == "" {
+            lotteryLabel.text = "$0"
+        } else {
+            if let oand = Float(lotteryJackpot.text!) {
+            lotteryLabel.text = oand.asLocalCurrency
+            updateData()
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+   
+    func calcs(){
+        
+        let payments = 30
+        
+        let annuityValue: Double = Double(lotteryJackpot.text!)!
+        
+        var initialPayment:Double {
+            return annuityValue * selectedLottery.initialPercent
+        }
+        var totalValue: Double = initialPayment
+        var annuityPayment: Double = initialPayment
+
+        
+        if stepperControl.value > 1 {
+            firstPayment = Float(initialPayment * 0.71)/Float(stepperControl.value)
+        } else {
+            firstPayment = Float(initialPayment * 0.71)
+        }
+        
+         allPayments = "Payment 1: \(firstPayment.asLocalCurrency)\n"
+        
+        func calcPayments() {
+            for i in 1..<payments {
+                annuityPayment = annuityPayment * selectedLottery.percentIncrease
+                totalValue += annuityPayment;
+                
+                if stepperControl.value > 1 {
+                    printInstallment = Float(annuityPayment * 0.71)/Float(stepperControl.value)
+                } else {
+                    printInstallment = Float(annuityPayment * 0.71)
+                }
+
+                currentPayment = " \(self.printInstallment.asLocalCurrency) \n"
+               // print(currentPayment)
+                allPayments = allPayments + currentPayment
+                
+                // append to array
+                installmentPayments.append(allPayments)
+            }
+            
+            //print(">>>>>>\(totalValue)")
+        }
+        
+        calcPayments()
+
+    annuityText.text = allPayments
+    
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+
+        
+    }
+
+    
+     func theAlert() {
+        
+        let anAlert = UIAlertController(title: "fuck me", message: "now", preferredStyle: .alert)
+        let anAction = UIAlertAction(title: "do it", style: .default, handler: nil)
+        anAlert.addAction(anAction)
+        self.present(anAlert, animated: true, completion: nil)
+        
+    }
+    
+    
+}
+
